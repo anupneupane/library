d843f75d02a7351f452ef596f8e5594809a9b910
Rahul Seshan <rahul.seshan@gmail.com>
2013-03-20 16:59:23 -0400

    partial up form

--- app/controllers/topic_links_controller.rb
+++ app/controllers/topic_links_controller.rb
@@ -84,4 +84,14 @@ class TopicLinksController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def up_vote
+    raise params.inspect
+    @topic_link = TopicLink.find(params[:id])
+    @topic_link.increase_score
+  end
+
+  def down_vote
+  end
+
 end
