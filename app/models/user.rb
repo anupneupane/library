542e98f132bfb2d2a03df2097b3834511169bcec
Rahul Seshan <rahul.seshan@gmail.com>
2013-03-22 15:08:03 -0400

    some validation and password hiding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessible :username, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation
 
   validates_uniqueness_of :username
+  validates_uniqueness_of :email
+  validates_presence_of :username
+  validates_presence_of :email
 
   has_many :votes
   has_many :topic_links, :through => :votes
