80e12ecbed321663f6ccf2175dd0f67e9676530e
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-03-22 23:28:32 -0400

    Improve user validations to make uniqueness not case sensitive and to check format of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,21 @@
 class User < ActiveRecord::Base
-  has_secure_password
   attr_accessible :username, :email, :password, :password_confirmation
 
-  validates_uniqueness_of :username
+  has_many :votes
+  has_many :topic_links, :through => :votes
+
+  has_secure_password
+  
+  validates_uniqueness_of :username, :case_sensitive => false
   validates_presence_of :username
+  validates_format_of :username, :with => /\A[A-Za-z0-9][A-Za-z0-9_-]*\Z/
   validates :username, :length => { :minimum => 3, :maximum => 15 }
 
   validates :password, :length => { :minimum => 4, :maximum => 20 }
 
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
   validates_presence_of :email
   validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
 
-
-  has_many :votes
-  has_many :topic_links, :through => :votes
   
 end
