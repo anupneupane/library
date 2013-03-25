e23e818d61572453b242ec3460bf862e5f26feba
Laura Brown <laurabrown1113@gmail.com>
2013-03-25 17:07:39 -0400

    trying to fix bug in migration file

--- db/migrate/20130324183148_drop_description_from_links.rb
+++ db/migrate/20130324183148_drop_description_from_links.rb
@@ -1,6 +1,6 @@
 class DropDescriptionFromLinks < ActiveRecord::Migration
   def change
-    remove_column :links, :title, :string
-    remove_column :links, :description, :text
+    remove_column :links, :title
+    remove_column :links, :description
   end
 end
