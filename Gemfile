7d721ce32d7453ecf439c4713b37c734d2904131
Laura Brown <laurabrown1113@gmail.com>
2013-03-25 15:17:26 -0400

    configuring

--- Gemfile
+++ Gemfile
@@ -5,16 +5,19 @@ gem 'rails', '3.2.12'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+gem 'jquery-rails'
 gem 'bcrypt-ruby'
 
 group :development do 
   gem 'pry-rails'
   gem 'awesome_print'
-  #gem 'irbtools'
-  #gem 'terminal-notifier'
-  #gem 'boson-more'
+  gem 'sqlite3'
 end
+
+group :production do
+  gem 'pg'
+end
+
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -31,7 +34,6 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+set :application, "library-app"
+set :repository,  "git@github.com:flatiron-school/library.git"
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :user, 'deploy'
+set :deploy_to, "/home/#{user}/#{application}"
+
+role :web, "198.211.103.217"                          # Your HTTP server, Apache/etc
+role :app, "198.211.103.217"                          # This may be the same as your `Web` server
+role :db,  "198.211.103.217", :primary => true # This is where Rails migrations will run
+
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
