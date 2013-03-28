b65261fb742ccfb94e12ce4853d002553b595b47
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-03-27 20:07:40 -0400

    Remove double flash notice from main page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       login(user)
-      redirect_to root_path, notice: "Logged in!"
+      redirect_to root_path, notice: "Logged In!"
     else
       flash.now[:notice] = "Email or password is invalid"
       render "new"
