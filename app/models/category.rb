8425fa565a5564575ec0c04125b0e469a5c4484c
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-03-28 09:51:18 -0400

    Comment out unused methods in Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,18 +3,21 @@ class Category < ActiveRecord::Base
   has_many :topics
   validates_presence_of :title
 
-  # Never used in controller or view.  Remove?
+  #Both methods were originally added to interface with the app
+  #in the terminal. They are never used in controller or view. 
+  #Remove?
+  #
   # def add_new_topic(title)
   #   self.topics.build(:title => title)
   #   self.save
   # end
 
-  def associate_topics(*topics)
-    topics = topics.flatten
-    topics.each do |t|
-      topic = Topic.find(t) 
-      self.topics << topic
-    end
-    self.save
-  end
+  # def associate_topics(*topics)
+  #   topics = topics.flatten
+  #   topics.each do |t|
+  #     topic = Topic.find(t) 
+  #     self.topics << topic
+  #   end
+  #   self.save
+  # end
 end
