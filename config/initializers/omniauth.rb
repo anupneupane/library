9a37bc0258a3ab7f3624be7891c4a7ce91c0b515
Laura Brown <laurabrown1113@gmail.com>
2013-04-03 18:13:45 -0400

    twitter auth prob

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 TWITCONFIG = YAML.load_file(Rails.root.join("config/twitter.yml"))[Rails.env]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, TWITCONFIG['consumer_key'], TWITCONFIG['consumer_token']
+  provider :twitter, 'uBbQMkAZI1eRI7kJSzBzA', '7Ol8c6U5CrYdqqDqOTKxOyDA9XDoGE8Fn4248zoOD4'
 end
