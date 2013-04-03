12d95874548f5684b8387722af23840be45ef1ce
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-04-03 15:13:37 -0400

    Remove unused methods from topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -10,17 +10,12 @@ class Topic < ActiveRecord::Base
 
   validates_presence_of :title
 
-  def has_best_link?
-    self.best_link != nil && self.best_link.score > 0
-  end
-
   def authorize?(user)
     user && (user.admin? || (self.user_id==user.id && has_no_topic_links?))
   end
 
-  def add_new_link(url)
-    self.links.build(:url => url)
-    self.save
+  def has_best_link?
+    self.best_link != nil && self.best_link.score > 0
   end
 
   def has_no_topic_links?
@@ -31,43 +26,8 @@ class Topic < ActiveRecord::Base
     true if (self.topic_links.where(:link_id => link.id).first) || false
   end
 
-  def reject_or_associate(link)
-    if self.includes_link?(link)
-      raise link.inspect
-      return 0
-    else #if it is not, make the association
-      self.topic_links.build(:link => link)
-      return 10
-    end
-  end
-
   def order_topic_links_by_score
     self.topic_links.all(:order => "score desc, updated_at desc")
   end
-
-  #  def associate_links(*links)
-  #   links = links.flatten
-  #   links.each do |l| 
-  #     self.links << l
-  #   end
-  #   self.save
-  # end
-
-   # def best_link
-  #   TopicLink.find_by_id(self.best_link_id)
-  # end
-
-  # def best_link=(topic_link_instance)
-  #   self.best_link_id = topic_link_instance.id
-  #   self.save
-  # end
-
-  #   if topic_link_instance.topic == self
-  #     self.best_link = topic_link_instance
-  #     self.save
-  #   else
-  #     raise "This link doesn't belong to this topic"
-  #   end
-  # end
 
 end
