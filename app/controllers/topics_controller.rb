7ead2151faddc29bf02050e8b93817632a1beb95
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-04-04 14:56:33 -0400

    Include sign up in modal

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -22,6 +22,7 @@ class TopicsController < ApplicationController
     @topic_link = TopicLink.new
     @link = @topic_link.build_link
     @request_url = {url: topic_links_path(@topic.id)}
+    @user = User.new unless current_user
 
     respond_to do |format|
       format.html # show.html.erb
